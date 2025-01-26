05ba657233b208d0f1a7b782b91eba47ce99b24a
Sayyam-Karnavat <sanyam.karnavat5@gmail.com>
2025-01-26 17:50:03 +0530

    added the functionality where the Screen and ICON persists and also the ICON and browser moves together when dragged across screen

--- python_app/latest.py
+++ python_app/latest.py
@@ -1,8 +1,8 @@
-from PySide6.QtCore import Qt, Signal, QPoint, QEvent
+from PySide6.QtCore import Qt, QPoint
 from PySide6.QtGui import QIcon, QMouseEvent
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QMenuBar, QMenu
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtGui import QAction
+
 
 class DraggableIcon(QWidget):
     def __init__(self):
@@ -25,74 +25,59 @@ class DraggableIcon(QWidget):
 
     def mouseMoveEvent(self, event: QMouseEvent):
         if self.start_position is not None and event.buttons() == Qt.LeftButton:
-            self.move(event.globalPos() - self.start_position)
+            new_position = event.globalPos() - self.start_position
+            self.move(new_position)
+
+            # Move the browser window together with the icon
+            if self.browser_window is not None and self.browser_window.isVisible():
+                self.browser_window.move(new_position.x() + self.width(), new_position.y())
 
     def mouseReleaseEvent(self, event: QMouseEvent):
         pass
 
     def mouseDoubleClickEvent(self, event: QMouseEvent):
-        self.transform_to_browser()
+        if self.browser_window is None:
+            self.transform_to_browser()
+        else:
+            self.browser_window.showNormal()  # Restore browser if minimized
+            self.browser_window.raise_()  # Bring it to the front
 
     def transform_to_browser(self):
-        self.hide()
-
-        # Create a larger geometry for the browser window
+        # Create a larger geometry for the browser window, placing it beside the icon
         x, y, _, _ = self.geometry().getRect()
         browser_geometry = self.geometry()
-        browser_geometry.setRect(x, y, 400, 400)  # Set default larger width and height
+        browser_geometry.setRect(x + self.width(), y, 400, 400)
 
-        self.browser_window = BrowserWindow(browser_geometry, self)  # Pass the DraggableIcon instance to BrowserWindow
+        self.browser_window = BrowserWindow(browser_geometry, self)  # Pass the icon instance
         self.browser_window.show()
 
-    def transform_to_icon(self):
-        self.show()
-        self.browser_window = None
+    def moveEvent(self, event):
+        if self.browser_window is not None and self.browser_window.isVisible():
+            # Ensure the browser window moves with the icon
+            self.browser_window.move(self.x() + self.width(), self.y())
+
+    def close_application(self):
+        QApplication.quit()  # Ensure the application closes fully
 
 
 class BrowserWindow(QMainWindow):
-    closed_signal = Signal()
-
     def __init__(self, geometry, draggable_icon):
         super().__init__()
-
-        self.setWindowFlags(Qt.Window)  # Allow default window behavior
+        self.setWindowFlags(Qt.Window | Qt.WindowStaysOnTopHint)
         self.setGeometry(geometry)
 
+        self.draggable_icon = draggable_icon  # Reference to the draggable icon
         self.is_dragging = False
         self.drag_position = QPoint()
-        self.draggable_icon = draggable_icon  # Save reference to DraggableIcon
 
-        # Set the default title "EverywearGPT" with company logo as the window icon
+        # Set the title and icon
         self.setWindowTitle("EverywearGPT")
         self.setWindowIcon(QIcon("company_logo.png"))  # Replace with your company logo
 
-        # Embed the browser
+        # Add the browser widget
         self.browser = QWebEngineView(self)
         self.browser.setUrl("https://chat.openai.com/")  # Set the URL
         self.setCentralWidget(self.browser)
-
-        # Create the menu bar
-        self.menu_bar = self.menuBar()
-        self.create_menu()
-
-    def create_menu(self):
-        # Add a menu to the title bar
-        file_menu = self.menu_bar.addMenu("File")
-
-        # Add actions to the menu
-        open_action = QAction("Open", self)
-        file_menu.addAction(open_action)
-
-        save_action = QAction("Save", self)
-        file_menu.addAction(save_action)
-
-        exit_action = QAction("Exit", self)
-        file_menu.addAction(exit_action)
-        exit_action.triggered.connect(self.close_application)
-
-    def close_application(self):
-        self.close()
-        QApplication.quit()
 
     def mousePressEvent(self, event: QMouseEvent):
         if event.button() == Qt.LeftButton:
@@ -101,23 +86,20 @@ class BrowserWindow(QMainWindow):
 
     def mouseMoveEvent(self, event: QMouseEvent):
         if self.is_dragging and event.buttons() == Qt.LeftButton:
-            self.move(event.globalPos() - self.drag_position)
+            new_position = event.globalPos() - self.drag_position
+            self.move(new_position)
 
     def mouseReleaseEvent(self, event: QMouseEvent):
         self.is_dragging = False
 
-    def closeEvent(self, event):
-        # Close the browser window and terminate the application
-        self.draggable_icon.close()  # Close the draggable icon
-        event.accept()  # Allow the window to close properly
-        QApplication.quit()  # Terminate the application
+    def moveEvent(self, event):
+        # Ensure the draggable icon moves with the browser window
+        if self.draggable_icon is not None:
+            self.draggable_icon.move(self.x() - self.draggable_icon.width(), self.y())
 
-    def changeEvent(self, event):
-        if event.type() == QEvent.WindowStateChange:
-            if self.windowState() == Qt.WindowMinimized:
-                self.hide()  # Hide the window when minimized
-                self.draggable_icon.transform_to_icon()  # Show the draggable icon
-        super().changeEvent(event)
+    def closeEvent(self, event):
+        # Close the entire application when the browser window is closed
+        self.draggable_icon.close_application()
 
 
 if __name__ == "__main__":
